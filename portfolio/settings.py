807fa0e28bb48c5f7f7c06a0450dccbc457acfde
ShivamGupta0 <ee3190601@iitd.ac.in>
2020-02-16 11:26:55 +0530

    kdo

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'portfoliodb',
         'USER':'postgres',
-        'PASSWORD':'Shivam@2000',
+        'PASSWORD':'**********',
         'HOST':'localhost',
         'PORT':'5432',
 
